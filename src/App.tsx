483a9e20c9fe893e5c8da1f94c3ed54df09c74c5
dee-sarania <dhritideepasarania@gmail.com>
2024-03-26 18:47:21 +0530

    navbar css added

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,70 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 // import Dashboard from './pages/patientDashboard';
-import Register from './pages/register';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import { Provider } from 'react-redux'
+import Register from "./pages/register";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
 import { store, persistor } from "./app/store";
 import { PersistGate } from "redux-persist/integration/react";
-import Home from './pages/home'
-import VitalsDashboard from './pages/vitalsDashboard';
-import Appointment from './pages/appointment';
-import PatientDashboard from './pages/patientDashboard';
-import DoctorDashboard from './pages/doctorDashboard';
+import Home from "./pages/home";
+import VitalsDashboard from "./pages/vitalsDashboard";
+import Appointment from "./pages/appointment";
+import PatientDashboard from "./pages/patientDashboard";
+import DoctorDashboard from "./pages/doctorDashboard";
 // import EcgGraph from './pages/test2'
 
 export const router = [
   {
     path: "/",
-    element: <Home/>,
+    element: <Home />,
   },
   {
     path: "/register",
-    element: <Register/>,
+    element: <Register />,
   },
   {
     path: "/patientdashboard",
-    element: <PatientDashboard/>,
+    element: <PatientDashboard />,
   },
   {
     path: "/vitals",
-    element: <VitalsDashboard/>
+    element: <VitalsDashboard />,
   },
   {
     path: "/appointment",
-    element: <Appointment/>
+    element: <Appointment />,
   },
   {
     path: "/patientdashboard",
-    element: <PatientDashboard/>,
+    element: <PatientDashboard />,
   },
   {
     path: "/doctordashboard",
-    element: <DoctorDashboard/>,
+    element: <DoctorDashboard />,
   },
   // {
   //   path: "/test2",
   //   element: <EcgGraph/>
   // }
-]
+];
 
 function App() {
   return (
-    <BrowserRouter>
-      <Provider store={ store }>
-        <PersistGate persistor={ persistor }>
-          <Routes>
-            {
-              router.map((item, index)=>{
-                return <Route path={item.path} element={item.element} />
-              })
-            }
-          </Routes>
-        </PersistGate>
-        </Provider>
-    </BrowserRouter>
+    <>
+      <div className="webcontainer">
+        <BrowserRouter>
+          <Provider store={store}>
+            <PersistGate persistor={persistor}>
+              <Routes>
+                {router.map((item, index) => {
+                  return <Route path={item.path} element={item.element} />;
+                })}
+              </Routes>
+            </PersistGate>
+          </Provider>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
