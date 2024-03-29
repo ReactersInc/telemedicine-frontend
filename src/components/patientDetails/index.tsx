570d1963aa926ee356c354f34781f531d3a6c09c
RAJVEER CHOUDHARY <rajveerjaat7300@gmail.com>
2024-03-30 00:49:51 +0530

    model remove

--- src/components/patientDetails/index.tsx
+++ src/components/patientDetails/index.tsx
@@ -4,21 +4,25 @@ import React, { useState } from "react";
 import jwt_decode, { JwtPayload } from "jwt-decode";
 import { useDispatch, useSelector } from "react-redux";
 import { userLogin } from "../../features/users/userSlice";
-import { setModal } from "../../features/modal/modalSlice";
-import ModalOTP from "../modleotp/modleotp";
-function PatientDetails() {
-  const isModalOpenotp = useSelector(
-    (state: { modal: { modalOpenotp: boolean; scrolliotp: boolean } }) =>
-      state.modal.modalOpenotp
-  );
-  const scrollable = useSelector(
-    (state: { modal: { modalOpenotp: boolean; scrollotp: boolean } }) =>
-      state.modal.scrollotp
-  );
-  const dispatch = useDispatch();
 
-  const toggleModal = () => {
-    dispatch(setModal());
+interface DecodedJwtPayload {
+  email: string;
+  dob: string;
+  exp: number;
+  gender: string;
+  name: string;
+  photourl: string;
+  state: string;
+  timestamp: string;
+}
+
+function PatientDetails() {
+  const [otp, setOtp] = useState("");
+  const [emailset, setemail] = useState("");
+  const [state, setstate] = useState(true);
+  const dispatch = useDispatch();
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setOtp(event.target.value);
   };
   const data = {
     email: "",
@@ -29,6 +33,60 @@ function PatientDetails() {
     photoURL: "",
     city: "",
     state: "",
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    const apiurl = "http://52.66.241.131/IoMTAppAPI/api/authOTP.php";
+    event.preventDefault();
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({email: emailset, otp: otp}),
+    };
+
+
+    try {
+      const response = await fetch(apiurl, requestOptions);
+
+      if (response.ok) {
+
+        const jsonResponse = await response.json();
+        if (jsonResponse && jsonResponse.Status) {
+
+          const jwtToken = jsonResponse.Status.jwt;
+          const decodedJwt: DecodedJwtPayload = jwt_decode(jwtToken);
+
+          dispatch(
+            userLogin({
+              email: decodedJwt.email,
+              dob: decodedJwt.dob,
+              exp: decodedJwt.exp,
+              gender: decodedJwt.gender,
+              name: decodedJwt.name,
+              photoUrl: decodedJwt.photourl,
+              state: decodedJwt.state,
+              timeStamp: decodedJwt.timestamp,
+            })
+          );
+          window.location.href = "patientdashboard";
+          data.email = "";
+          data.name = "";
+          data.dob = "";
+          data.gender = "";
+          data.password = "";
+          data.photoURL = "";
+          data.city = "";
+          data.state = "";
+          setOtp("");
+        } else {
+        }
+      } else {
+        window.alert("invalid email or password");
+      }
+    } catch (error) {
+    }
   };
   const signup = async () => {
     const fname = (document.getElementById("fname") as HTMLInputElement).value;
@@ -105,8 +163,8 @@ function PatientDetails() {
     data.photoURL = photoURL;
     data.city = city;
     data.state = state;
+    setemail(email);
 
-    console.log("initiating api...");
     const apiUrl = "http://52.66.241.131/IoMTAppAPI/api/addUser.php";
     const requestOptions = {
       method: "POST",
@@ -122,39 +180,25 @@ function PatientDetails() {
         const jsonResponse = await response.json();
         if (jsonResponse && jsonResponse.Status) {
           // window.location.href = "patientdashboard";
-          dispatch(setModal());
-          data.email = "";
-          data.name = "";
-          data.dob = "";
-          data.gender = "";
-          data.password = "";
-          data.photoURL = "";
-          data.city = "";
-          data.state = "";
+          setstate(false);
         } else {
-          console.error("Invalid response structure.");
         }
       } else {
-        console.error("Error sending JSON data:", response.statusText);
         window.alert("invalid email or password");
       }
     } catch (error) {
-      console.error("Error sending JSON data:", error);
     }
-    console.log(data);
   };
   return (
     <>
-      {isModalOpenotp ? (
-        <ModalOTP email={data.email} />
-      ) : (
+      {state ? (
         <div>
           <div className="flex justify-items-start space-x-48  mt-16 items-start">
             <div>
               <label htmlFor="fname">First Name *</label>
               <br />
               <input
-              autoComplete="off"
+                autoComplete="off"
                 type="text"
                 id="fname"
                 name="fname"
@@ -166,7 +210,7 @@ function PatientDetails() {
               <label htmlFor="lname">Last Name *</label>
               <br />
               <input
-              autoComplete="off"
+                autoComplete="off"
                 type="text"
                 id="lname"
                 name="lname"
@@ -184,6 +228,7 @@ function PatientDetails() {
                 className="style_input"
                 required
               >
+                <option value=""></option>
                 <option value="male">Male</option>
                 <option value="female">Female</option>
                 <option value="other">Other</option>
@@ -195,7 +240,7 @@ function PatientDetails() {
               <label htmlFor="email">Email *</label>
               <br />
               <input
-              autoComplete="off"
+                autoComplete="off"
                 type="email"
                 id="email"
                 name="email"
@@ -207,7 +252,7 @@ function PatientDetails() {
               <label htmlFor="mobile">Mobile Number *</label>
               <br />
               <input
-              autoComplete="off"
+                autoComplete="off"
                 type="text"
                 name="mobile"
                 id="mobile"
@@ -224,8 +269,8 @@ function PatientDetails() {
                 id="bloodGroup"
                 className="style_input w-full"
                 required
-                >
-                <option value="">Select...</option> 
+              >
+                <option value=""></option>
                 <option value="A+">A+</option>
                 <option value="A-">A-</option>
                 <option value="B+">B+</option>
@@ -255,7 +300,7 @@ function PatientDetails() {
               <label htmlFor="weight">Weight(kg) *</label>
               <br />
               <input
-              autoComplete="off"
+                autoComplete="off"
                 type="number"
                 name="weight"
                 id="weight"
@@ -294,7 +339,7 @@ function PatientDetails() {
               <label htmlFor="city">City *</label>
               <br />
               <input
-              autoComplete="off"
+                autoComplete="off"
                 type="text"
                 name="city"
                 id="city"
@@ -306,7 +351,7 @@ function PatientDetails() {
               <label htmlFor="state">State *</label>
               <br />
               <input
-              autoComplete="off"
+                autoComplete="off"
                 type="text"
                 name="state"
                 id="state"
@@ -318,7 +363,7 @@ function PatientDetails() {
               <label htmlFor="country">Country *</label>
               <br />
               <input
-              autoComplete="off"
+                autoComplete="off"
                 type="text"
                 name="country"
                 id="country"
@@ -332,8 +377,8 @@ function PatientDetails() {
               <label htmlFor="password">Password *</label>
               <br />
               <input
-              autoComplete="off"
-              type="password" 
+                autoComplete="off"
+                type="password"
                 id="password"
                 name="password"
                 className="style_input"
@@ -363,8 +408,39 @@ function PatientDetails() {
             Submit
           </button>
         </div>
-      )}{" "}
-     </>
+      ) : (
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div
+              className="fixed inset-0 transition-opacity"
+              aria-hidden="true"
+            >
+              <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+            </div>
+
+            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <form onSubmit={handleSubmit}>
+                  <input
+                    type="number"
+                    value={otp}
+                    onChange={handleInputChange}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    placeholder="Enter OTP"
+                  />
+                  <button
+                    type="submit"
+                    className="mt-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  >
+                    Verify OTP
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
