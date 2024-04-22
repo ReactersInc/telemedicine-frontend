1598d6771ba4bd57ac5f0d76b40880dfb2d38909
Vivek Rai <122623874+vivekrai9900@users.noreply.github.com>
2024-04-23 00:03:34 +0530

    navbar responsive done

--- src/components/horizontalNav/index.tsx
+++ src/components/horizontalNav/index.tsx
@@ -3,6 +3,7 @@ import "./index.css";
 import ModalSignin from "../modalSignin";
 import { useDispatch, useSelector } from "react-redux";
 import { setModal } from "../../features/modal/modalSlice";
+
 function HorizontalNav() {
   const isModalOpen = useSelector(
     (state: { modal: { modalOpen: boolean; scroll: boolean } }) =>
@@ -24,41 +25,75 @@ function HorizontalNav() {
     document.body.style.overflowY = "auto";
   }
 
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  // Function to scroll back to the top of the page
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth" // Optional, smooth scrolling animation
+    });
+  };
+
   return (
     <>
       <div className="navbar">
-        <div>
+        <div className="title">
           <a href="/#home">
-            <img src=".././static/logo.png" />
+            <img src=".././static/logo.png" alt="Logo" />
           </a>
         </div>
-        <div>
-          <a href="/#home" className="navLink">
-            Home
-          </a>
-          &nbsp;&nbsp; &nbsp;&nbsp;
-          <a href="/#service" className="navLink">
-            Service
-          </a>
-          &nbsp;&nbsp; &nbsp;&nbsp;
-          <a href="/#aboutUs" className="navLink">
-            About Us
-          </a>
-          &nbsp;&nbsp; &nbsp;&nbsp;
-          <a href="/#contact" className="navLink">
-            Contact
-          </a>
-          &nbsp;&nbsp; &nbsp;&nbsp;
-          <button className="signinBtn" onClick={toggleModal}>
-            Sign in
-          </button>
-          &nbsp;&nbsp;
-          <a href="/register">
-            <button className="SignBtn">Register</button>
-          </a>
+        <div
+          className="menu"
+          onClick={() => {
+            setMenuOpen(!menuOpen);
+          }}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
+
+        <ul className={menuOpen ? "open" : ""}>
+          <li>
+            <a href="/#home" className="navLink">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="/#service" className="navLink">
+              Service
+            </a>
+          </li>
+          <li>
+            <a href="/#aboutUs" className="navLink">
+              About Us
+            </a>
+          </li>
+          <li>
+            <a href="/#contact" className="navLink">
+              Contact
+            </a>
+          </li>
+          <li>
+            <button className="signinBtn" onClick={toggleModal}>
+              Sign in
+            </button>
+          </li>
+          <li>
+            <a href="/register">
+              <button className="SignBtn">Register</button>
+            </a>
+          </li>
+        </ul>
       </div>
+
       <div>{isModalOpen && <ModalSignin />}</div>
+
+      {/* Back to Top button */}
+      <button className="backToTopBtn" onClick={scrollToTop}>
+       <img src="https://media.geeksforgeeks.org/wp-content/uploads/20240227155250/up.png" alt="back to top" ></img>
+      </button>
     </>
   );
 }
