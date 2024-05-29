35187921c11e7f45211eaa666238f1b32426053e
dee-sarania <dhritideepasarania@gmail.com>
2024-05-29 11:36:19 +0530

    changes in upcoming appointments

--- src/pages/upcomming appointment/index.tsx
+++ src/pages/upcomming appointment/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
 import VerticalNavPatient from "../../components/verticalNavPatient";
 
 type Booking = {
@@ -16,6 +17,12 @@ type Doctor = {
   bookings: Booking[];
 };
 
+interface PrevProps {
+  Doc_ID: string;
+  P_Email: string;
+  Book_Date: string;
+}
+
 type ApiResponse = {
   Status: {
     message: string;
@@ -24,6 +31,18 @@ type ApiResponse = {
 };
 
 const UpcomingAppointment = () => {
+  const name = useSelector(
+    (state: {
+      user: {
+        email: string;
+        name: string;
+        gender: string;
+        dob: string;
+        city: string;
+        state: string;
+      };
+    }) => state.user
+  );
   const [data, setData] = useState<ApiResponse | null>(null);
 
   useEffect(() => {
@@ -32,7 +51,7 @@ const UpcomingAppointment = () => {
         const response = await axios.post<ApiResponse>(
           "https://makemytwin.com/IoMTAppAPI/api/upcomingBookingsPatient.php",
           {
-            patient_email: "csb21070@tezu.ac.in",
+            patient_email: name.email,
           }
         );
         setData(response.data);
@@ -56,41 +75,42 @@ const UpcomingAppointment = () => {
     <>
       <VerticalNavPatient />
       <div className="sm:ml-12 md:ml-24 lg:ml-32 xl:ml-48 mt-10">
-  <h1 className="text-xl sm:text-2xl font-bold mb-4 text-blue-600">
-    {data.Status.message}
-  </h1>
-  {data.Status.information &&
-    data.Status.information.map((doctor, index) => (
-      <div
-        key={index}
-        className="mb-4 p-4 bg-white shadow-lg rounded-lg sm:flex sm:justify-between sm:items-center"
-      >
-        <div>
-          <h2 className="text-lg sm:text-xl font-semibold text-blue-500">
-            {doctor.doctor_name}
-          </h2>
-          <p className="text-gray-600">{doctor.doctor_email}</p>
-          <p className="text-gray-600">{doctor.doctor_mobileno}</p>
-          <p className="text-gray-600">{doctor.doctor_specilization}</p>
-        </div>
-        <div>
-          <h3 className="font-semibold text-blue-500">Bookings:</h3>
-          {doctor.bookings &&
-            doctor.bookings.map((booking, index) => (
-              <div
-                key={index}
-                className="p-2 mt-2 bg-gray-100 rounded sm:flex sm:justify-between sm:items-center"
-              >
-                <p className="text-gray-700">Date: {booking.Date}</p>
-                <p className="text-gray-700">Time: {booking.Time}</p>
-                <p className="text-gray-700">Slot Number: {booking.Slot_Number}</p>
+        <h1 className="text-xl sm:text-2xl font-bold mb-4 text-blue-600">
+          {data.Status.message}
+        </h1>
+        {data.Status.information &&
+          data.Status.information.map((doctor, index) => (
+            <div
+              key={index}
+              className="mb-4 p-4 bg-white shadow-lg rounded-lg sm:flex sm:justify-between sm:items-center"
+            >
+              <div>
+                <h2 className="text-lg sm:text-xl font-semibold text-blue-500">
+                  {doctor.doctor_name}
+                </h2>
+                <p className="text-gray-600">{doctor.doctor_email}</p>
+                <p className="text-gray-600">{doctor.doctor_mobileno}</p>
+                <p className="text-gray-600">{doctor.doctor_specilization}</p>
               </div>
-            ))}
-        </div>
+              <div>
+                <h3 className="font-semibold text-blue-500">Bookings:</h3>
+                {doctor.bookings &&
+                  doctor.bookings.map((booking, index) => (
+                    <div
+                      key={index}
+                      className="p-2 mt-2 bg-gray-100 rounded sm:flex sm:justify-between sm:items-center flex-col"
+                    >
+                      <p className="text-gray-700">Date: {booking.Date}</p>
+                      <p className="text-gray-700">Time: {booking.Time}</p>
+                      <p className="text-gray-700">
+                        Slot Number: {booking.Slot_Number}
+                      </p>
+                    </div>
+                  ))}
+              </div>
+            </div>
+          ))}
       </div>
-    ))}
-</div>
-
     </>
   );
 };
